5aec53e52997594f0f0f48aa180544bc409d37b3
Henrique Dreyer <henriquedreyer@gmail.com>
2021-12-12 12:40:56 -0300

    Unit Test

--- src/app/sale-taxes/checkout/checkout-item/checkout-item.component.spec.ts
+++ src/app/sale-taxes/checkout/checkout-item/checkout-item.component.spec.ts
@@ -1,21 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { MockForUnitTestService } from 'src/app/shared/mock-for-unit-test.service';
+import { StoreModule } from '@ngrx/store';
 
 import { CheckoutItemComponent } from './checkout-item.component';
+import { AppState } from '../../state/app.state';
 
 describe('CheckoutItemComponent', () => {
   let component: CheckoutItemComponent;
   let fixture: ComponentFixture<CheckoutItemComponent>;
+  let store: MockStore;
+  let mockService: MockForUnitTestService;
+
+  const initialState: AppState = {
+    myCart: [],
+    productList: [],
+    checkout: [],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckoutItemComponent ]
-    })
-    .compileComponents();
+      declarations: [CheckoutItemComponent],
+      providers: [
+        MockForUnitTestService,
+        HttpClient,
+        provideMockStore({ initialState: initialState })
+      ],
+      imports: [],
+    }).compileComponents();
+    store = TestBed.inject(MockStore);
+    mockService = TestBed.inject(MockForUnitTestService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CheckoutItemComponent);
     component = fixture.componentInstance;
+    component.product = mockService.builderProduct();
+    component.index = 0;
     fixture.detectChanges();
   });
 
